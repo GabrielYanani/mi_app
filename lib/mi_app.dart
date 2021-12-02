b1167973493f1bb39431e12d9cc64e7280ebdd16
GabrielYanani <gabyyanani@gmail.com>
2021-12-02 18:52:52 -0300

    mi app 1.1.3

--- lib/mi_app.dart
+++ lib/mi_app.dart
@@ -12,13 +12,36 @@ class MiApp1 extends StatefulWidget {
 }
 
 class _MiApp1State extends State<MiApp1> {
-  int _counter = 0;
-
+  // List misFotos=[ Image miFoto = Image.asset('assets/januka.jpg',),
+  // Image miPrimeraFoto = Image.asset('assets/jaim.jpg'),
+  // Image miSegundaFoto = Image.asset('assets/tobe.jpg'),];
+  Image miFoto = Image.asset('assets/januka.jpg',);
+  Image miPrimeraFoto = Image.asset('assets/jaim.jpg');
+  Image miSegundaFoto = Image.asset('assets/tobe.jpg');
+// child: new ListView(
+//           children: [
+//             Image.asset('images/carimg.jpeg',  
+//               width: 600.0,
+//               height: 240.0,
+//               fit: BoxFit.cover,),
+//             Image.asset('images/carimg.jpeg',  
+//               width: 600.0,
+//               height: 240.0,
+//               fit: BoxFit.cover,),
+//             Image.asset('images/carimg.jpeg',  
+//               width: 600.0,
+//               height: 240.0,
+//               fit: BoxFit.cover,),
+//             Image.asset('images/carimg.jpeg',  
+//               width: 600.0,
+//               height: 240.0,
+//               fit: BoxFit.cover,)
+//           ],
   get child => null;
 
   void _mostrarFoto() {
     setState(() {
-      _counter++;
+      miFoto = miPrimeraFoto;
     });
   }
 
@@ -26,42 +49,49 @@ class _MiApp1State extends State<MiApp1> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("BIENVENIDOS A MI"),
+        title: Text(
+          'חנוכה שמח!!',
+          textDirection: TextDirection.rtl,
+          style: TextStyle(fontSize: 40),
+        ),
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
       backgroundColor: Colors.cyan,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Container(
-              color: Colors.red,
-              child: Text(
-                'חנוכה שמח!!',
-                textDirection: TextDirection.rtl,
-                style: TextStyle(
-                    fontSize: 70,
-                    decoration: TextDecoration.underline,
-                    fontStyle: FontStyle.italic),
-              ),
+              color: Colors.blue,
+              // child: Text(
+              //   'חנוכה שמח!!',
+              //   textDirection: TextDirection.rtl,
+              //   style: TextStyle(
+              //       fontSize: 70,
+              //       decoration: TextDecoration.underline,
+              //       fontStyle: FontStyle.italic),
+              // ),
             ),
-            Text('Asi festejamos en casa!',
-                style: TextStyle(
-                  fontSize: 40,
-                  color: Colors.white,
-                )),
-            Image.network(
-              "https://photos.google.com/photo/AF1QipNQRhuQ-CYyRAWjGMU8xlVz2lh5N96g99OQxgU2",
-              scale: 1.0,
-            )
+            Text(
+              'Asi festejamos en casa!',
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Expanded(
+              child: miFoto,
+            ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _mostrarFoto,
-        tooltip: 'Increment',
+        tooltip: 'CAmbiarFoto',
         child: const Icon(Icons.photo),
+        backgroundColor: Colors.amber,
       ),
     );
   }
