d1e14746b86218016a71f6fbf87fbaa8ab9dbe1e
GabrielYanani <gabyyanani@gmail.com>
2021-12-04 23:11:45 -0300

    + list imagens

--- lib/mi_app.dart
+++ lib/mi_app.dart
@@ -3,16 +3,14 @@
 import 'package:flutter/material.dart';
 
 class MiApp1 extends StatefulWidget {
-  const MiApp1({Key? key}) : super(key: key);
+  const MiApp1({Key key}) : super(key: key);
 
   @override
   State<MiApp1> createState() => _MiApp1State();
 }
 
 class _MiApp1State extends State<MiApp1> {
-  // List misFotos=[Image.asset('assets/januka.jpg'),
-  // Image.asset('assets/jaim.jpg'),
-  // Image.asset('assets/tobe.jpg'),];
+ 
   Image miFoto = Image.asset('assets/januka.jpg');
   Image miPrimeraFoto = Image.asset('assets/jaim.jpg');
   Image miSegundaFoto = Image.asset('assets/tobe.jpg');
@@ -42,35 +40,34 @@ class _MiApp1State extends State<MiApp1> {
         centerTitle: true,
       ),
       backgroundColor: Colors.cyan,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Container(
-              color: Colors.blue,
-            ),
-            Text(
-              text1,
-              style: TextStyle(
-                fontSize: 40,
-                color: Colors.white,
-                fontStyle: FontStyle.italic,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            Expanded(
-              // child: ListView=ListView('assets/misFotos'),
-              child: miFoto,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _cambiarFotoTexto,
-        tooltip: 'CAmbiarFoto',
-        child: const Icon(Icons.photo),
-        backgroundColor: Colors.amber,
-      ),
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //     children: <Widget>[
+      //       Container(
+      //         color: Colors.blue,
+      //       ),
+      //       Text(
+      //         text1,
+      //         style: TextStyle(
+      //           fontSize: 40,
+      //           color: Colors.white,
+      //           fontStyle: FontStyle.italic,
+      //         ),
+      //         textAlign: TextAlign.center,
+      //       ),
+      //       Expanded(
+      //         child: miFoto,
+      //       ),
+      //     ],
+      //   ),
+      // ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _cambiarFotoTexto,
+      //   tooltip: 'CAmbiarFoto',
+      //   child: const Icon(Icons.photo),
+      //   backgroundColor: Colors.amber,
+      // ),
     );
   }
 }
